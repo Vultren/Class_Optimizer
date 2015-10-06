e01e94014cfb1ec18458463998aa575ca9fc3803
Eric Walden <Eric.Walden@1901group.com>
2015-10-06 12:19:16 -0400

    project set -up

---
+++ ClassOptimizer/src/main/java/edu/gatech/optimizer/ClassOptimizerApplication.java
@@ -0,0 +1,12 @@
+package edu.gatech.optimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassOptimizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClassOptimizerApplication.class, args);
+    }
+}
---
+++ ClassOptimizer/src/test/java/edu/gatech/optimizer/ClassOptimizerApplicationTests.java
@@ -0,0 +1,18 @@
+package edu.gatech.optimizer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ClassOptimizerApplication.class)
+@WebAppConfiguration
+public class ClassOptimizerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
